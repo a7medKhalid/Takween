82f5ea328f7cb3c43a7b0f997e132d7690f11544
a7medKhalid <69413330+a7medKhalid@users.noreply.github.com>
2022-08-23 03:13:25 +0300

    add toast

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,14 @@
 
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
+                @if(Auth::user()->subscriptionType === 'free')
+            <div class="flex items-center justify-center w-screen h-10 bg-white">
+                <div class="flex items-center justify-center  max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <svg class="w-6 h-6 text-yellow-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path></svg>
+                    You can upgrade your account from <a class="ml-1 underline hover:text-blue-500" href="{{ route('Subscriptions') }}"> subscriptions page</a>
+                </div>
+            </div>
+                @endif
 
             <!-- Page Heading -->
             @if (isset($header))
--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -284,7 +284,7 @@
                 <p class="font-light text-gray-500 ">Aceses all databses from anywhere.</p>
             </div>
             <div>
-                <svg class="w-10 h-10 mb-2 text-slate-600 md:w-12 md:h-12 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg>
+                <svg class="w-10 h-10 mb-2 text-slate-600 md:w-12 md:h-12 "fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z" clip-rule="evenodd"></path></svg>
                 <h3 class="mb-2 text-2xl font-bold ">Faster shipping</h3>
                 <p class="font-light text-gray-500 ">Start working on your app faster</p>
             </div>
@@ -317,7 +317,7 @@
 <section class="bg-white ">
     <div class="max-w-screen-xl px-4 py-8 mx-auto lg:py-24 lg:px-6">
         <div class="max-w-screen-md mx-auto mb-8 text-center lg:mb-12">
-            <h2 class="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 ">Designed for developers like you</h2>
+            <h2 class="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 ">Designed for developers</h2>
             <p class="mb-5 font-light text-gray-500 sm:text-xl ">Multiple subscription types designed for your needs.</p>
         </div>
         <div class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
